9e5083705a982a87db0fc277c4c990415129d410
amkessler <kessler.aaron@gmail.com>
2016-03-07 14:26:15 -0500

    added initial intro files for integers, strings, lists

---
+++ part1/1_workingwintegers.py
@@ -0,0 +1,43 @@
+
+#INTEGERS
+
+#In programming, an integer is a whole number. You can do all sorts of math on integers,
+#just like you'd do with a calculator or in Excel or a database manager.
+
+2+2
+
+5*5
+
+# Order of operations also works the same as real-world math you learned back in grade school
+
+2+2*10
+
+# ...is different than 
+
+(2+2)*10
+
+# we can create a variable for a number - which is a name that stands in for the actual integer
+
+mynumber = 14
+
+#Once you have a variable, you can run math on it by using its name like you would the number
+
+# Multiplication
+mynumber * 5
+
+# Division
+mynumber / 2
+
+# Addition
+mynumber + 20
+
+# Subtraction
+mynumber - 5
+
+# You can get as complex as you want with these...
+(mynumber*2+56)/100
+
+
+#you can idenifty what TYPE your variable is like this 
+type(mynumber)
+
