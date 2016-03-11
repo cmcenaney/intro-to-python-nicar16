c7ccb9476dbaad1b5d2b45e976aacb35a129aa87
Aaron Williams <aaron.colby.williams@gmail.com>
2016-03-11 14:31:39 -0500

    use dicts

--- part3/1_analysis.py
+++ part3/1_analysis.py
@@ -1,7 +1,14 @@
+"""
+First, we'll import several standard library modules to use. These are some of the most frequently used libaries
+in Python. Also, these three double quotes--you can use single quotes too--create a multi-line string. It's great
+For adding a large comment like this one or for setting a variable to a large block of text.
+"""
 import os
 import csv
 import json
 
+
+# Use the os module to 
 file_name = "marijuana_gross_sales.csv"
 file_path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'data', file_name))
 
@@ -14,17 +21,30 @@ csv_data = csv.reader(csv_file)
 # Create a variable to represent the header row
 header_row = csv_data.next()  # explain generators
 
+# transforming data
 data = list(csv_data)
 
-all_gross_income = [d[3] for d in data]  # this will fail
+# analyze data
+all_gross_income = [d[3] for d in data]
+# sum(all_gross_income)  # this will fail
+def list_to_dict(alist):
+	# alist vs list
+	return dict( zip(header_row, alist) )
 
-# sum(all_gross_income)  # error
+list_to_dict = lambda alist: dict( zip(header_row, alist) )
 
-all_gross_income = [float(d[3]) for d in data]
+data_dict = [list_to_dict(d) for d in data]
+
+for d in data_dict:
+	d['GROSS_SALES'] = float(d['GROSS_SALES'])
+	d['MONTH'] = d['MONTH'].strip()
+
+all_gross_income = [d['GROSS_SALES'] for d in data_dict]  # reads better!
+
 average_gross_income =  sum(all_gross_income) / len(all_gross_income)
 
-data_2015 = [d for d in data if d[0] == '2015']
-all_gross_income_2015 = [float(d[3]) for d in data_2015]
+data_2015 = [d for d in data_dict if d['YEAR'] == '2015']
+all_gross_income_2015 = [d['GROSS_SALES'] for d in data_2015]
 average_gross_income_2015 = sum(all_gross_income_2015) / len(all_gross_income_2015)
 
 # data_2014 = [d for d in data if d[0] == '2014']
@@ -36,20 +56,32 @@ average_gross_income_2015 = sum(all_gross_income_2015) / len(all_gross_income_20
 
 # lambda value:float(value)
 
-largest_sale = max(data, key=lambda x:float(x[3]))
-smallest_sale = min(data, key=lambda x:float(x[3]))
+largest_sale = max(data_dict, key=lambda x:float(x['GROSS_SALES']))
+smallest_sale = min(data_dict, key=lambda x:float(x['GROSS_SALES']))
 
-pct_of_sales = float(largest_sale[3]) / sum(all_gross_income_2015) * 100
+pct_of_sales = largest_sale['GROSS_SALES'] / sum(all_gross_income_2015) * 100
 
-def list_to_dict(alist):
-	# alist vs list
-	return dict( zip(header_row, alist) )
+sum_of_gross_income = sum(all_gross_income)
 
-list_to_dict = lambda alist: dict( zip(header_row, alist) )
+def robotext(sum_of_gross_income, largest_sale, pct_of_sales):
+	format_numbers = lambda x: "{:,}".format(x)
+	# sum_of_gross_income = "{:,}".format(sum_of_gross_income)
+	sum_of_gross_income = format_numbers(sum_of_gross_income)
+	largest_sale['GROSS_SALES'] = format_numbers(largest_sale['GROSS_SALES'])
+	pct_of_sales = round(pct_of_sales)
 
-data_dict = [list_to_dict(d) for d in data]
+	print """
+	People are smoking the pot in Colorado.
 
+	The state has earned a total ${} since legalizing marijuana in 2014.
+	Sales boomed in {} {} when total sales reached ${}--{} percent of all sales that year.
+	""".format(sum_of_gross_income, largest_sale['MONTH'].capitalize(), largest_sale['YEAR'], largest_sale['GROSS_SALES'], pct_of_sales)
+
+robotext(sum_of_gross_income, largest_sale, pct_of_sales)
+
+
+# wriite to different file format
 with open('marijuana_gross_sales.json', 'wb') as outfile:
-	json_data = json.dumps(data_dict)
+	json_data = json.dumps(data_dict, indent=4)
 	outfile.write(json_data)
 
